fc3f3702917432a92d60659a90c786912feff518
Mitch Alforque <mitch@vananaz.com>
2024-02-12 17:09:45 +0800

    Set default password in UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,7 +15,7 @@ class UserFactory extends Factory
     /**
      * The current password being used by the factory.
      */
-    protected static ?string $password;
+    protected static ?string $password = 'P@ssw0rd';
 
     /**
      * Define the model's default state.
