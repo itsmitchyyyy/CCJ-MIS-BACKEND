324a227ab2e4203a4815e0c27e1de898c58a5752
Mitch Alforque <mitch@vananaz.com>
2024-03-16 16:14:59 +0800

    Add fillable and relationship to Subject model in SubjectStudents model

--- app/Models/SubjectStudents.php
+++ app/Models/SubjectStudents.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Subject;
 
 class SubjectStudents extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'subject_id'];
+
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
 }
