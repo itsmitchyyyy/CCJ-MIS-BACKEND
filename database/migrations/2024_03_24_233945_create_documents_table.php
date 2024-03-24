5b80e9df621685eda148ebde962b627635c7b6a8
Mitch Alforque <mitch@vananaz.com>
2024-03-24 23:43:01 +0800

    Add Document model and migration

---
+++ database/migrations/2024_03_24_233945_create_documents_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['office', 'student'])->default('office');
+            $table->string('name');
+            $table->string('file_path');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents');
+    }
+};
