db2905e1c557f2d538dffbaba5ad4ca9aa69ab85
Mitch Alforque <mitch@vananaz.com>
2024-04-17 22:37:29 +0800

    Add index and destroy methods to FacilityController, update routes/api.php

--- app/Http/Controllers/FacilityController.php
+++ app/Http/Controllers/FacilityController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreFacilityRequest;
+use App\Http\Resources\FacilityResource;
 use App\Models\Facility;
 
 class FacilityController extends Controller
@@ -13,5 +14,25 @@ class FacilityController extends Controller
         $data = $request->validated();
         $facility = Facility::create($data);
         return response()->json($facility, 201);
+   }
+
+   public function index(Request $request)
+   {
+        $facilities = Facility::when($request->type, function ($query, $type) {
+            return $query->where('type', $type);
+        })
+        ->when($request->status, function ($query, $status) {
+            return $query->where('status', $status);
+        })
+        ->get();
+
+        FacilityResource::withoutWrapping();
+        return FacilityResource::collection($facilities);
+   }
+
+   public function destroy(Facility $facility)
+   {
+        $facility->delete();
+        return response()->noContent();
    }
 }
