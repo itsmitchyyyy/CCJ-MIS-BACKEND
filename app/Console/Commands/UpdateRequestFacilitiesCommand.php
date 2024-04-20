2c2a78356d28a3d90414fa9d3aaa7ab236683d40
Mitch Alforque <mitch@vananaz.com>
2024-04-21 00:04:44 +0800

    Update request facility status for expired reservations

--- app/Console/Commands/UpdateRequestFacilitiesCommand.php
+++ app/Console/Commands/UpdateRequestFacilitiesCommand.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use App\Models\RequestFacility;
 use App\Enums\FacilityStatus;
 use App\Enums\FacilityType;
+use App\Enums\RequestFacilityStatus;
 use Carbon\Carbon;
 
 class UpdateRequestFacilitiesCommand extends Command
@@ -35,6 +36,9 @@ class UpdateRequestFacilitiesCommand extends Command
         foreach ($requestFacilities as $requestFacility) {
            if ($requestFacility->facility->type != FacilityType::Equipment && Carbon::now()->gt(Carbon::parse($requestFacility->reservation_date))) {
                $requestFacility->facility()->update(['status' => FacilityStatus::Available]);
+               if ($requestFacility->status == RequestFacilityStatus::Pending) {
+                   $requestFacility->update(['status' => RequestFacilityStatus::Rejected]);
+               }
            }
         }
     }
